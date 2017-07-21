e81c4fc257e99d3b3d032afcea2a732273a72dd1
Michael extends Object <github@meo.codes>
2017-07-21 23:53:51 +0200

    Errors as default response for all operations

--- api/src/main/java/codes/meo/restspringboot/swagger/SwaggerApi.java
+++ api/src/main/java/codes/meo/restspringboot/swagger/SwaggerApi.java
@@ -1,5 +1,6 @@
 package codes.meo.restspringboot.swagger;
 
+import codes.meo.restspringboot.error.Error;
 import io.swagger.annotations.*;
 
 import javax.ws.rs.GET;
@@ -16,7 +17,8 @@ public interface SwaggerApi {
     @Path("/{fileName}.json")
     @ApiOperation("Get the Swagger specification")
     @ApiResponses(value = {
-            @ApiResponse(code = 404, message = "Swagger specification not found")
+            @ApiResponse(code = 404, message = "Swagger specification not found"),
+            @ApiResponse(code = 0, message = "One or more errors occurred", response = Error.class, responseContainer = "List")
     })
     String getSwaggerJson(
             @ApiParam(value = "Name of the Swagger specification file", required = true)
